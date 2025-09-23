c91f9e9b7a51a6c0e774fbeebd06003ec6e74f03
Sebastian Bonilla <jsbonilla@unicauca.edu.co>
2025-09-23 13:36:59 -0500

    v4 pruebas

--- src/main/java/edu/unicauca/reportes_SIVRI/service/ProyectoService.java
+++
@@ -1,9 +0,0 @@
-package edu.unicauca.reportes_SIVRI.service;
-
-import org.springframework.stereotype.Service;
-import edu.unicauca.reportes_SIVRI.dto.ProyectoDTO;
-
-@Service
-public interface ProyectoService extends ReporteService<ProyectoDTO> {
-    // otros metodos especificos...
-}
--- src/main/java/edu/unicauca/reportes_SIVRI/service/ReporteService.java
+++
@@ -1,5 +0,0 @@
-package edu.unicauca.reportes_SIVRI.service;
-
-public interface ReporteService<T> {
-    byte[] generarReporte(String tipoReporte, T datos);
-}
